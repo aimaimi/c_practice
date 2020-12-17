a6525e7c4988e73a0fe5da2e97317a626adc044d
aimaimi <yh1127@hotmail.co.jp>
2020-12-17 16:49:39 +0900

    自作関数

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
+void disp(int num){
+    printf("%d\n",num);
+}
+
+int add(int a, int b){
+    return a+b;
+}
+
 int main() {
 
-    unsigned char c = 200;
-    printf("%d\n",c);
+    disp(55);
+    disp(add(20,50));
+    int a, b;
+    a = 3;
+    b = 5;
+
+    if(a < b){
+        printf("aはbより小さいです");
+    }
 
     return 0;
 }
