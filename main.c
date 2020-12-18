9afe3f2fd6c42f7a1c6130851f159cd4dca998f5
aimaimi <yh1127@hotmail.co.jp>
2020-12-18 11:53:55 +0900

    for文

--- main.c
+++ main.c
@@ -13,13 +13,27 @@ int main() {
 
     disp(55);
     disp(add(20,50));
-    int a, b;
-    a = 3;
+    int a, b,count;
     b = 5;
+//    scanf("%d",&a);
 
-    if(a < b){
-        printf("aはbより小さいです");
+    for(count = 0; count < 7;count++){
+        printf("%d回目\n",count+1);
     }
+
+
+//    switch(a){
+//        case 1:
+//            printf("aは1です");
+//            break;
+//        case 2:
+//            printf("aは2です");
+//            break;
+//        default:
+//            printf("どれにも当てはまりません");
+//    }
+
+//    a < b ? printf("aはbより小さいです") : printf("aはbより小さくないです");
 
     return 0;
 }
